70364bde05791960e10085b1622ec3ea363d528e
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-15 18:30:54 -0800

    Add input text and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { MdSend } from 'react-icons/md'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 text-white">
       <p> Ink Vision </p>
+      <div className="flex flex-row items-end justify-between w-full max-w-md">
+        <Input
+          placeholder="Write your text here"
+          className="flex-grow p-2 rounded-md"
+        />
+        <Button>
+          <MdSend />
+        </Button>
+      </div>
     </main>
-  )
+  );
 }
