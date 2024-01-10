86e7f40e2b164d792a791e9ddc4c4ff372fa1d69
aripo99 <a_polakof@umail.ucsb.edu>
2024-01-10 08:38:20 -0800

    Add create a new tattoo button

--- app/creation/page.tsx
+++ app/creation/page.tsx
@@ -2,7 +2,8 @@
 
 import { useSearchParams } from 'next/navigation'
 import Image from 'next/image';
-
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 export default function Page() {
     const searchParams = useSearchParams();
     const imageUrl = searchParams.get('image');
@@ -14,16 +15,23 @@ export default function Page() {
     }
 
     return (
-        <div className='flex flex-col items-center p-24'>
-            <h1 className='p-8'>
-                {prompt}
-            </h1>
-            <Image
-                src={imageUrl}
-                alt="Generated from API"
-                width={500}
-                height={500}
-            />
-        </div>
+        <>
+            <div className='flex flex-col items-center p-24'>
+                <h1 className='p-8'>
+                    {prompt}
+                </h1>
+                <Image
+                    src={imageUrl}
+                    alt="Generated from API"
+                    width={500}
+                    height={500}
+                />
+                <Link href="/">
+                    <Button className="m-12">
+                        Create a new tattoo
+                    </Button>
+                </Link>
+            </div>
+        </>
     );
 }
