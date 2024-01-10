fbf5c2f9ec1a90de14ca762dccb579cc7901dea9
aripo99 <a_polakof@umail.ucsb.edu>
2024-01-10 07:51:23 -0800

    Add spinner

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -5,13 +5,16 @@ import { Button } from '@/components/ui/button'
 import { MdSend } from 'react-icons/md'
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Spinner from '@/components/Spinner';
 
 const PromptInput = () => {
     const [inputValue, setInputValue] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        setIsLoading(true);
         try {
             const response = await fetch('/api/generate', {
                 method: 'POST',
@@ -23,26 +26,30 @@ const PromptInput = () => {
             if (!response.ok) {
                 throw new Error(`Error: ${response.status}`);
             }
-            // Assuming you want to do something with the response:
             const data = await response.json();
 
             router.push(`/creation?image=${encodeURIComponent(data[0].url)}&prompt=${encodeURIComponent(inputValue)}`);
         } catch (error) {
             console.error('Failed to submit prompt', error);
+        } finally {
+            setIsLoading(false);
         }
     };
     return (
-        <form onSubmit={handleSubmit} className="flex flex-row items-end justify-between w-full max-w-md mb-8">
-            <Input
-                placeholder="Describe your tattoo"
-                className="flex-grow p-2 rounded-md"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-            />
-            <Button type="submit">
-                <MdSend />
-            </Button>
-        </form>
+        isLoading ?
+            <Spinner />
+            :
+            <form onSubmit={handleSubmit} className="flex flex-row items-end justify-between w-full max-w-md mb-8">
+                <Input
+                    placeholder="Describe your tattoo"
+                    className="flex-grow p-2 rounded-md"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <Button type="submit">
+                    <MdSend />
+                </Button>
+            </form>
     )
 }
 
