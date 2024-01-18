4da000ef413677107c4cc1130d78dac8b74c30e2
aripo99 <a_polakof@umail.ucsb.edu>
2024-01-17 20:06:32 -0800

    Make images unoptimized to fix 502 error

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const nextConfig = {
             pathname: '/**', // This is a wildcard pattern to match all paths in your domain
         },
         ],
-    },
+        unoptimized: true
+    }
 }
 
 module.exports = nextConfig
