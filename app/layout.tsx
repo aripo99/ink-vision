2ccee86cff5997e9ada88dabcf9ddf3f2dba180c
aripo99 <a_polakof@umail.ucsb.edu>
2024-01-17 20:25:26 -0800

    Add analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import Link from 'next/link'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             <p className="text-3xl p-8">Ink Vision</p>
           </Link>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
